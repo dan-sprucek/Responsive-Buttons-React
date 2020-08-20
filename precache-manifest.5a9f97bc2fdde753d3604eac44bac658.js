a9fc994ce5654eb335105d4a159263854459b6ca
dan-sprucek <dan.sprucek@gmail.com>
2020-08-20 19:06:46 +0200

    Updates

---
+++ precache-manifest.5a9f97bc2fdde753d3604eac44bac658.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5458fc8e0498e0e3367afbe1dc1712ad",
+    "url": "/Responsive-Buttons-React/index.html"
+  },
+  {
+    "revision": "c4380481dbaa6b288f2c",
+    "url": "/Responsive-Buttons-React/static/css/main.16d9f762.chunk.css"
+  },
+  {
+    "revision": "6bf3ab3c83e297b9cf40",
+    "url": "/Responsive-Buttons-React/static/js/2.691a5a87.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Responsive-Buttons-React/static/js/2.691a5a87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4380481dbaa6b288f2c",
+    "url": "/Responsive-Buttons-React/static/js/main.274c56c7.chunk.js"
+  },
+  {
+    "revision": "ca6c724043acc8b233f3",
+    "url": "/Responsive-Buttons-React/static/js/runtime-main.1f507eb5.js"
+  }
+]);
